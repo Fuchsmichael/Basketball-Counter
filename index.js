a12d4bb488fc04abf08c54723910f51021e26fb9
Fuchsmichael <m_pehlivan@icloud.com>
2025-06-29 15:12:48 +0200

    Basketball Scorer with CSS fonts html

---
+++ index.js
@@ -0,0 +1,34 @@
+let homeScore = 0
+let guestScore = 0
+
+function addHome1() {
+    homeScore += 1
+    document.getElementById("home").textContent = homeScore
+}
+
+function addHome2() {
+    homeScore += 2
+    document.getElementById("home").textContent = homeScore
+}
+
+function addHome3() {
+    homeScore += 3
+    document.getElementById("home").textContent = homeScore
+}
+
+function addGuest1() {
+    guestScore += 1
+    document.getElementById("guest").textContent = guestScore
+}
+
+
+
+function addGuest2() {
+    guestScore += 2
+    document.getElementById("guest").textContent = guestScore
+}
+
+function addGuest3() {
+    guestScore += 3
+    document.getElementById("guest").textContent = guestScore
+}
